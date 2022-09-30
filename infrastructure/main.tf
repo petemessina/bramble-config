685f1b58a0976c45dc869671dec4e9adf48b0082
petemessina <peter.j.messina@gmail.com>
2022-09-30 16:29:54 -0400

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,15 +70,7 @@ resource "helm_release" "traefik" {
   name             = "traefik"
   repository       = "https://helm.traefik.io/traefik"
   chart            = "traefik"
-  
-  # If default_values == "" then apply default values from the chart if its anything else 
-  # then apply values file using the values_file input variable
-  values = [""]
-
-  set {
-    name = "deployment.replicas"
-    value = 1
-  }
+  timeout          = 800
 
   depends_on = [
     kubernetes_namespace.traefik_namespace
